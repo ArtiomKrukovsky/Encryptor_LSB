db46d72a2fa49f5a1594ee2b345389968013fa8e
Artiom Krukovsky <Artiomgood1@gmail.com>
2019-05-01 10:35:04 +0300

    Encryptor_LSB

---
+++ Lab_10_LCD_v2/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace Lab_10_LCD_v2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox_bmp = new System.Windows.Forms.PictureBox();
+            this.btn_Open_bmp_encrypt = new System.Windows.Forms.Button();
+            this.btn_Open_bmp_decrypt = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_Open_txt_encrypt = new System.Windows.Forms.Button();
+            this.textBox_fileName_encrypt = new System.Windows.Forms.TextBox();
+            this.btn_Encrypt = new System.Windows.Forms.Button();
+            this.btn_Decrypt = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_bmp)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LightGray;
+            this.panel1.Controls.Add(this.btn_Encrypt);
+            this.panel1.Controls.Add(this.textBox_fileName_encrypt);
+            this.panel1.Controls.Add(this.btn_Open_txt_encrypt);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.btn_Open_bmp_encrypt);
+            this.panel1.Location = new System.Drawing.Point(2, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(254, 138);
+            this.panel1.TabIndex = 0;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.BackColor = System.Drawing.Color.Silver;
+            this.flowLayoutPanel1.Controls.Add(this.pictureBox_bmp);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(262, 1);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(307, 291);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.LightGray;
+            this.panel2.Controls.Add(this.btn_Decrypt);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.btn_Open_bmp_decrypt);
+            this.panel2.Location = new System.Drawing.Point(2, 145);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(254, 147);
+            this.panel2.TabIndex = 0;
+            // 
+            // pictureBox_bmp
+            // 
+            this.pictureBox_bmp.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox_bmp.InitialImage = null;
+            this.pictureBox_bmp.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox_bmp.Name = "pictureBox_bmp";
+            this.pictureBox_bmp.Size = new System.Drawing.Size(304, 288);
+            this.pictureBox_bmp.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_bmp.TabIndex = 0;
+            this.pictureBox_bmp.TabStop = false;
+            // 
+            // btn_Open_bmp_encrypt
+            // 
+            this.btn_Open_bmp_encrypt.Location = new System.Drawing.Point(53, 27);
+            this.btn_Open_bmp_encrypt.Name = "btn_Open_bmp_encrypt";
+            this.btn_Open_bmp_encrypt.Size = new System.Drawing.Size(156, 23);
+            this.btn_Open_bmp_encrypt.TabIndex = 0;
+            this.btn_Open_bmp_encrypt.Text = "Загрузить контейнет .bmp";
+            this.btn_Open_bmp_encrypt.UseVisualStyleBackColor = true;
+            this.btn_Open_bmp_encrypt.Click += new System.EventHandler(this.btn_Open_bmp_encrypt_Click);
+            // 
+            // btn_Open_bmp_decrypt
+            // 
+            this.btn_Open_bmp_decrypt.Location = new System.Drawing.Point(53, 38);
+            this.btn_Open_bmp_decrypt.Name = "btn_Open_bmp_decrypt";
+            this.btn_Open_bmp_decrypt.Size = new System.Drawing.Size(156, 23);
+            this.btn_Open_bmp_decrypt.TabIndex = 0;
+            this.btn_Open_bmp_decrypt.Text = "Загрузить контейнет .bmp";
+            this.btn_Open_bmp_decrypt.UseVisualStyleBackColor = true;
+            this.btn_Open_bmp_decrypt.Click += new System.EventHandler(this.btn_Open_bmp_decrypt_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Maroon;
+            this.label1.Location = new System.Drawing.Point(6, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 19);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Шифрование";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.Maroon;
+            this.label2.Location = new System.Drawing.Point(6, 5);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Расшифрование";
+            // 
+            // btn_Open_txt_encrypt
+            // 
+            this.btn_Open_txt_encrypt.Enabled = false;
+            this.btn_Open_txt_encrypt.Location = new System.Drawing.Point(10, 59);
+            this.btn_Open_txt_encrypt.Name = "btn_Open_txt_encrypt";
+            this.btn_Open_txt_encrypt.Size = new System.Drawing.Size(119, 21);
+            this.btn_Open_txt_encrypt.TabIndex = 2;
+            this.btn_Open_txt_encrypt.Text = "Открыть txt файл";
+            this.btn_Open_txt_encrypt.UseVisualStyleBackColor = true;
+            this.btn_Open_txt_encrypt.Click += new System.EventHandler(this.btn_Open_txt_encrypt_Click);
+            // 
+            // textBox_fileName_encrypt
+            // 
+            this.textBox_fileName_encrypt.Enabled = false;
+            this.textBox_fileName_encrypt.Location = new System.Drawing.Point(147, 60);
+            this.textBox_fileName_encrypt.Name = "textBox_fileName_encrypt";
+            this.textBox_fileName_encrypt.Size = new System.Drawing.Size(104, 20);
+            this.textBox_fileName_encrypt.TabIndex = 3;
+            // 
+            // btn_Encrypt
+            // 
+            this.btn_Encrypt.BackColor = System.Drawing.Color.Tomato;
+            this.btn_Encrypt.Enabled = false;
+            this.btn_Encrypt.Location = new System.Drawing.Point(71, 97);
+            this.btn_Encrypt.Name = "btn_Encrypt";
+            this.btn_Encrypt.Size = new System.Drawing.Size(107, 28);
+            this.btn_Encrypt.TabIndex = 4;
+            this.btn_Encrypt.Text = "Зашифровать";
+            this.btn_Encrypt.UseVisualStyleBackColor = false;
+            this.btn_Encrypt.Click += new System.EventHandler(this.btn_Encrypt_Click);
+            // 
+            // btn_Decrypt
+            // 
+            this.btn_Decrypt.BackColor = System.Drawing.Color.PaleGreen;
+            this.btn_Decrypt.Enabled = false;
+            this.btn_Decrypt.Location = new System.Drawing.Point(22, 76);
+            this.btn_Decrypt.Name = "btn_Decrypt";
+            this.btn_Decrypt.Size = new System.Drawing.Size(107, 28);
+            this.btn_Decrypt.TabIndex = 4;
+            this.btn_Decrypt.Text = "Расшифровать";
+            this.btn_Decrypt.UseVisualStyleBackColor = false;
+            this.btn_Decrypt.Click += new System.EventHandler(this.btn_Decrypt_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(568, 291);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Lab_10_LCD";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_bmp)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox_bmp;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_Open_bmp_encrypt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_Open_bmp_decrypt;
+        private System.Windows.Forms.TextBox textBox_fileName_encrypt;
+        private System.Windows.Forms.Button btn_Open_txt_encrypt;
+        private System.Windows.Forms.Button btn_Encrypt;
+        private System.Windows.Forms.Button btn_Decrypt;
+    }
+}
+
---
+++ Lab_10_LCD_v2/Form1.cs
@@ -0,0 +1,313 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Lab_10_LCD_v2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static char[] alfavit = new char[] { ' ','0','1','2','3','4','5','6','7','8','9', 'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ь', 'ы', 'ъ', 'э', 'ю', 'я' };
+        Bitmap bmp;
+        #region Methods EnCrypt
+        /// <summary>
+        /// Get Hash
+        /// </summary>
+        /// <param name="M_int"></param>
+        /// <param name="H0"></param>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public static int H(List<int> M_int, int H0, int n)
+        {
+            int Hi = H0;
+            for (int i = 0; i < M_int.Count; i++)
+            {
+                int H_rez = Convert.ToInt32(Math.Pow(Hi + M_int[i], 2) % n);
+                Console.WriteLine("H" + (i + 1) + "= " + H_rez);
+                Hi = H_rez;
+            }
+            return Hi;
+        }
+
+        /// <summary>
+        /// Open picture.bmp
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_Open_bmp_encrypt_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                bmp = new Bitmap(ofd.FileName, true);
+                pictureBox_bmp.Image = bmp;
+                btn_Open_txt_encrypt.Enabled = true;
+            }
+        }
+
+        string encrypt_text = null;
+        /// <summary>
+        /// Open file.txt 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_Open_txt_encrypt_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog ofd = new OpenFileDialog();
+                ofd.InitialDirectory = @"Desktop";
+                ofd.RestoreDirectory = true;
+                ofd.FileName = @"";
+                ofd.Filter = "Все форматы|*.txt;";
+                string m_f = null; string Path = null;
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    Path = ofd.FileName;
+                    textBox_fileName_encrypt.Text = ofd.FileName.Remove(0, ofd.FileName.LastIndexOf('\\') + 1);
+                    m_f = File.ReadAllText(Path).ToLower();
+                    encrypt_text = m_f.ToLower();
+                    char[] mass = m_f.ToCharArray();
+                    List<int> M_int = new List<int>();
+                    foreach (var item in mass)
+                        M_int.Add(Array.IndexOf(alfavit, item));
+                    int p = 19; int q = 17; int n = p * q; int H0 = 100;
+                    int Hash_obraz = H(M_int, H0, n);
+                    File.AppendAllText(Path, Hash_obraz.ToString());
+                    encrypt_text += Hash_obraz.ToString();
+                    btn_Encrypt.Enabled = true;
+                }
+            }
+            catch { }
+        }
+
+        public enum State
+        {
+            Hiding,
+            Filling_With_Zeros
+        };
+
+        /// <summary>
+        /// Button Encrypt
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_Encrypt_Click(object sender, EventArgs e)
+        {
+            if (encrypt_text != null)
+            { 
+                embedText(encrypt_text, bmp).Save("EncryptPicture.bmp");
+                btn_Open_txt_encrypt.Enabled = true;
+                btn_Encrypt.Enabled = true;
+                textBox_fileName_encrypt.Clear();
+                MessageBox.Show("Шифрование прошло успешно...");
+            }
+            else
+            {
+                MessageBox.Show("Отсутствует сообщения для шифрования!", "Ошибка шифрации");
+            }
+        }
+
+        /// <summary>
+        /// Метод embedText in picture.bmp
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="bmp"></param>
+        /// <returns></returns>
+        public static Bitmap embedText(string text, Bitmap bmp)
+        {
+            State state = State.Hiding;
+
+            int charIndex = 0;
+            int charValue = 0;
+            long pixelElementIndex = 0;
+            int zeros = 0;
+            int R = 0, G = 0, B = 0;
+
+            for (int i = 0; i < bmp.Height; i++)
+            {
+                for (int j = 0; j < bmp.Width; j++)
+                {
+                    Color pixel = bmp.GetPixel(j, i);
+                    R = pixel.R - pixel.R % 2;
+                    G = pixel.G - pixel.G % 2;
+                    B = pixel.B - pixel.B % 2;
+                    for (int n = 0; n < 3; n++)
+                    {
+                        if (pixelElementIndex % 8 == 0)
+                        {
+                            if (state == State.Filling_With_Zeros && zeros == 8)
+                            {
+                                if ((pixelElementIndex - 1) % 3 < 2)
+                                {
+                                    bmp.SetPixel(j, i, Color.FromArgb(R, G, B));
+                                }
+                                return bmp;
+                            }
+                            if (charIndex >= text.Length)
+                            {
+                                state = State.Filling_With_Zeros;
+                            }
+                            else
+                            {
+                                charValue = text[charIndex++];
+                            }
+                        }
+
+                        switch (pixelElementIndex % 3)
+                        {
+                            case 0:
+                                {
+                                    if (state == State.Hiding)
+                                    {
+                                        R += charValue % 2;
+                                        charValue /= 2;
+                                    }
+                                }
+                                break;
+                            case 1:
+                                {
+                                    if (state == State.Hiding)
+                                    {
+                                        G += charValue % 2;
+                                        charValue /= 2;
+                                    }
+                                }
+                                break;
+                            case 2:
+                                {
+                                    if (state == State.Hiding)
+                                    {
+                                        B += charValue % 2;
+                                        charValue /= 2;
+                                    }
+                                    bmp.SetPixel(j, i, Color.FromArgb(R, G, B));
+                                }
+                                break;
+                        }
+                        pixelElementIndex++;
+                        if (state == State.Filling_With_Zeros)
+                        {
+                            zeros++;
+                        }
+                    }
+                }
+            }
+            return bmp;
+        }
+        #endregion
+
+        /// <summary>
+        /// Метод extractText from picture.bmp
+        /// </summary>
+        /// <param name="bmp"></param>
+        /// <returns></returns>
+        public static string extractText(Bitmap bmp)
+        {
+            int colorUnitIndex = 0;
+            int charValue = 0;
+
+            string extractedText = String.Empty;
+            for (int i = 0; i < bmp.Height; i++)
+            {
+                for (int j = 0; j < bmp.Width; j++)
+                {
+                    Color pixel = bmp.GetPixel(j, i);
+                    for (int n = 0; n < 3; n++)
+                    {
+                        switch (colorUnitIndex % 3)
+                        {
+                            case 0:
+                                {
+                                    charValue = charValue * 2 + pixel.R % 2;
+                                }
+                                break;
+                            case 1:
+                                {
+                                    charValue = charValue * 2 + pixel.G % 2;
+                                }
+                                break;
+                            case 2:
+                                {
+                                    charValue = charValue * 2 + pixel.B % 2;
+                                }
+                                break;
+                        }
+                        colorUnitIndex++;
+                        if (colorUnitIndex % 8 == 0)
+                        {
+                            charValue = reverseBits(charValue);
+                            if (charValue == 0)
+                            {
+                                return extractedText;
+                            }
+                            char c = (char)charValue;
+                            extractedText += c.ToString();
+                        }
+                    }
+                }
+            }
+            return extractedText;
+        }
+        /// <summary>
+        /// reverseBits
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public static int reverseBits(int n)
+        {
+            int result = 0;
+            for (int i = 0; i < 8; i++)
+            {
+                result = result * 2 + n % 2;
+                n /= 2;
+            }
+            return result;
+        }
+
+
+
+        /// <summary>
+        /// Open picture.bmp
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_Open_bmp_decrypt_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                bmp = new Bitmap(ofd.FileName, true);
+                pictureBox_bmp.Image = bmp;
+                btn_Decrypt.Enabled = true;
+            }
+        }
+
+        string Decrypt_text = null;
+        /// <summary>
+        /// Button Decrypt
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_Decrypt_Click(object sender, EventArgs e)
+        {
+            Decrypt_text = extractText(bmp);
+            btn_Decrypt.Enabled = false;
+            FileStream f = new FileStream("Decrypt.txt", FileMode.Create);
+            StreamWriter wr = new StreamWriter(f);
+            wr.WriteLine(Decrypt_text);
+            wr.Close();
+            MessageBox.Show("Расшифрование прошло успешно... Проверьте ваши данные");
+        }
+    }
+}
